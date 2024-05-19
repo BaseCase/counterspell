175d5d7350052ff52e84d13d4d8b76b052327b74
Casey Brant <casey@caseybrant.com>
2024-05-18 19:51:06 -0500

    simplify grid data and generation code

--- src/main/counterspell/core.cljs
+++ src/main/counterspell/core.cljs
@@ -5,7 +5,8 @@
 (def game-turns 3)
 
 ;; TODO: how big should the grid be?
-(def grid-size {:rows 5 :cols 4})
+(def grid-rows 5)
+(def grid-cols 4)
 
 ;; TODO: figure out how to get seeds working
 (defn create-letter-generator [seed]
@@ -15,34 +16,23 @@
 (def letters (create-letter-generator nil))
 
 
-;; Gives us columns as a list of lists of maps.
+;; Gives us columns as a list of lists of letters
 ;; We'll render the columns bottom to top; that is, first item is lowest of the screen.
-;; TODO: could this function be less terrible?
 (defn generate-game-grid []
-  (let [height (* (grid-size :rows) (inc game-turns))
-        width (grid-size :cols)
-        flat-letters (take (* height width) letters)
-        build-starting-maps (fn [letters]
-                              (map #(hash-map :letter % :visible false) letters))
-        set-initial-visible (fn [xs]
-                              (concat
-                               (map #(assoc % :visible true) (take (grid-size :rows) xs))
-                               (drop (grid-size :rows) xs)))]
-
+  (let [height (* grid-rows (inc game-turns))
+        flat-letters (take (* height grid-cols) letters)]
     (->> flat-letters
-         (partition height)
-         (map build-starting-maps)
-         (map set-initial-visible))))
-
+         (partition height))))
 
 (defn letter-grid []
   ;; TODO: the grid probably wants to be a ratom
   (let [grid (generate-game-grid)]
     [:div.letter-grid
      (for [col grid]
-       [:div.column
-        (for [letter (->> col (filter :visible) reverse)]
-          [:div.letter (:letter letter)])])]))
+       (let [letters (take grid-rows col)]
+         [:div.column
+          (for [l letters]
+            [:div.letter l])]))]))
 
 (defn main []
   [:div
