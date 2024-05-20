1b53f3bedd66f5153ac88ba27728ab56b6db934b
Casey Brant <casey@caseybrant.com>
2024-05-19 21:27:05 -0500

    correct rules for selecting/deselecting tiles; "submit" word plays

--- src/main/counterspell/core.cljs
+++ src/main/counterspell/core.cljs
@@ -28,23 +28,76 @@
 
 
 (def state (r/atom {:grid (generate-game-grid)
-                    :active-tiles #{}}))
-(defn activate-tile [x y]
-  (swap! state update-in [:active-tiles] conj [x y]))
+                    :active-tiles []
+                    :last-submitted-word nil}))
+
+(defn tile-active? [x y]
+  (some #(= % [x y]) (@state :active-tiles)))
+
+(defn tiles-to-string [tiles grid]
+  (apply str (map (fn [[x y]]
+                    (nth (nth grid x) y))
+                  tiles)))
+
+(defn submit-word! [tiles]
+  (swap! state (fn [s]
+                 (let [word (tiles-to-string tiles (s :grid))]
+                   (-> s
+                       (assoc :last-submitted-word word)
+                       (assoc :active-tiles []))))))
+
+(defn legal-tiles []
+  (let [tail (last (@state :active-tiles))]
+    (if tail
+      ;; if we have an active selection, legal tiles are any inactive tiles adjacent to the tail
+      (let [[x y] tail]
+        (-> (for [x' (range (dec x) (+ 2 x))
+                  y' (range (dec y) (+ 2 y))]
+              (when (and (not (tile-active? x' y'))
+                         (<= 0 x')
+                         (<= 0 y')
+                         (< x' grid-cols)
+                         (< y' grid-rows))
+                [x' y']))
+            set
+            (disj nil)))
+      ;; if there is no selection, any tile is a legal move
+      (-> (for [x (range grid-cols)
+                y (range grid-rows)]
+            [x y])
+          set))))
+
+(defn tile-action-at! [x y]
+  "takes coords of a clicked tile and figures out what to do with it: update active selection and maybe submit a word."
+  (let [active-tiles (@state :active-tiles)]
+    (if (tile-active? x y)
+      (if (= [x y] (last active-tiles))
+        ;; if you click the last tile twice, submit it as a word
+        (submit-word! active-tiles)
+        ;; if you click the active chain somewhere in its middle, deselect anything past that
+        (swap! state assoc :active-tiles
+               (conj (->> active-tiles
+                          (take-while #(not (= % [x y])))
+                          (into []))
+                     [x y])))
+      (if ((legal-tiles) [x y])
+        ;; if this tile is inactive and valid, activate it!
+        (swap! state assoc :active-tiles
+               (conj active-tiles [x y]))
+        ;; if you click an invalid inactive tile, reset the selection
+        (swap! state assoc :active-tiles
+               [])))))
 
 ;;
 ;; ui components
 ;;
 (defn letter-tile [{:keys [letter x y]}]
-  (let [on-click-letter (fn [evt]
-                          (activate-tile x y))
-        activated? ((@state :active-tiles) [x y])]
+  (let [activated? (tile-active? x y)]
     [:div.letter {:class (when activated? "active")
-                  :on-click on-click-letter}
+                  :on-click #(tile-action-at! x y)}
      letter]))
 
 (defn letter-grid []
-  ;; TODO: the grid probably wants to be a ratom
   (let [grid (:grid @state)]
     [:div.letter-grid
      (for [[x col] (index grid)]
@@ -54,10 +107,23 @@
             [letter-tile {:letter l :x x :y y
                           :key (str l x y)}])]))]))
 
+(defn building-word []
+  (let [word (tiles-to-string (@state :active-tiles) (@state :grid))]
+    [:div.building-word
+     [:h3 word]]))
+
+(defn submitted-words []
+  (let [word (@state :last-submitted-word)]
+    (when (and (empty? (@state :active-tiles)) word)
+      [:div.submitted-word
+       [:h3 (str word " is a word!")]])))
+
 (defn main []
   [:div
    [:h1 "Counterspell!!!"]
-   [letter-grid]])
+   [letter-grid]
+   [building-word]
+   [submitted-words]])
 
 (defonce root (rdom/create-root (.querySelector js/document "#root")))
 
